9839b235ae324badf088e0d1f57a9eaa4f1fa157
ValiSpac <spacvalentin1@gmail.com>
2026-02-10 14:38:11 +0100

    p1 working well

--- P1/scripts/config_worker.sh
+++ P1/scripts/config_worker.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-# Get the master node's IP from the arguments
-MASTER_IP=$1
-
+apt-get update -y
+apt-get install -y curl ca-certificates gnupg lsb-release iptables iproute2 net-tools
 # Get the token from the shared folder
 TOKEN=$(cat /vagrant/token)
 
 # Install K3s agent (worker) and join the master node
-curl -sfL https://get.k3s.io | K3S_URL=https://$MASTER_IP:6443 K3S_TOKEN=$TOKEN sh -
+curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$TOKEN INSTALL_K3S_EXEC="--flannel-iface=enp0s8" sh -
